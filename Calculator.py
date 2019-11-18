ba116b0a3ef45840994990c4b2d6ed1c748138da
blazeris <blazeris@users.noreply.github.com>
2019-11-17 18:40:32 -0800

    operations dictionary and use of find instead of iteratives in recursion

--- Calculator.py
+++ Calculator.py
@@ -1,15 +1,27 @@
 input = input()
 
+def add(firstTerm, secondTerm):
+    return firstTerm + secondTerm
 
-def calculate(inputLine)
+def subtract(firstTerm, secondTerm):
+    return firstTerm - secondTerm
+
+def multiply(firstTerm, secondTerm):
+    return firstTerm * secondTerm
+
+def divide(firstTerm, secondTerm):
+    return firstTerm / secondTerm
+
+def calculate(inputLine):
     inputLine = inputLine.replace(" ","")
-    open = -1
-    close = -1
-
-    for i in range(inputLine.len() - 1):
-        if inputLine[i] == '(':
-            open = i
-        if inputLine[i] == ')':
-            close = i
-            inputLine = inputLine[0,open] + calculate(inputLine[open + 1,close]) + inputLine(close + 1, inputLine.len())
+    operations = {'+': add,
+                  '-': subtract,
+                  '*': multiply,
+                  '/': divide
+                  }
+    open = inputLine.find('(')
+    if open is not -1:
+        reversed = inputLine[::-1] #string slicing to reverse
+        close = reversed.find(')')
+        inputLine = inputLine[0, open] + calculate(inputLine[open + 1, input.len() - close - 1]) + inputLine(input.len() - close - 1, inputLine.len())
 print(calculate(input))
