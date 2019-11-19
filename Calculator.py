b1bc76fdae5b02caad3dc9db76af57cfbf32c33e
blazeris <blazeris@users.noreply.github.com>
2019-11-18 23:27:33 -0800

    figured how to do math, will need to rewrite to support multi-digit numbers

--- Calculator.py
+++ Calculator.py
@@ -1,27 +1,49 @@
 input = input()
 
 def add(firstTerm, secondTerm):
-    return firstTerm + secondTerm
+    return str(int(firstTerm) + int(secondTerm))
 
 def subtract(firstTerm, secondTerm):
-    return firstTerm - secondTerm
+    return str(int(firstTerm) - int(secondTerm))
 
 def multiply(firstTerm, secondTerm):
-    return firstTerm * secondTerm
+    return str(int(firstTerm) * int(secondTerm))
 
 def divide(firstTerm, secondTerm):
-    return firstTerm / secondTerm
+    return str(int(firstTerm) / int(secondTerm))
 
 def calculate(inputLine):
     inputLine = inputLine.replace(" ","")
-    operations = {'+': add,
-                  '-': subtract,
-                  '*': multiply,
-                  '/': divide
-                  }
+
     open = inputLine.find('(')
     if open is not -1:
         reversed = inputLine[::-1] #string slicing to reverse
         close = reversed.find(')')
         inputLine = inputLine[0, open] + calculate(inputLine[open + 1, input.len() - close - 1]) + inputLine(input.len() - close - 1, inputLine.len())
+
+    done = False
+    while done is False:
+        done = True
+        operatorIndex = inputLine.find('*' or '/')
+        if operatorIndex is not -1:
+            operationSubstring = str(inputLine[operatorIndex - 1: operatorIndex + 2])
+            done = False
+            if inputLine[operatorIndex] is '*':
+                inputLine = inputLine.replace(operationSubstring, multiply(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
+                print(inputLine)
+            elif inputLine[operatorIndex] is '/':
+                inputLine = inputLine.replace(operationSubstring, divide(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
+                print(inputLine)
+        else:
+            operatorIndex = inputLine.find('+' or '-')
+            if operatorIndex is not -1:
+                operationSubstring = str(inputLine[operatorIndex - 1: operatorIndex + 2])
+                done = False
+                if inputLine[operatorIndex] is '+':
+                    inputLine = inputLine.replace(operationSubstring, add(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
+                    print(inputLine)
+                elif inputLine[operatorIndex] is '-':
+                    inputLine = inputLine.replace(operationSubstring, subtract(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
+                    print(inputLine)
+    return inputLine
 print(calculate(input))
