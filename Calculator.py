921950242ed52b6d0b1ceaf4ea99cefa83894707
blazeris <blazeris@users.noreply.github.com>
2019-11-19 10:19:55 -0800

    made recursive parenthesis handling work

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,3 @@
-input = input()
-
 def add(firstTerm, secondTerm):
     return str(int(firstTerm) + int(secondTerm))
 
@@ -14,36 +12,40 @@ def divide(firstTerm, secondTerm):
 
 def calculate(inputLine):
     inputLine = inputLine.replace(" ","")
-
+    operationOrder = {'*', '/', '+', '-'}
+    operations = {'*': multiply,
+                  '/': divide,
+                  '+': add,
+                  '-': subtract
+    }
     open = inputLine.find('(')
     if open is not -1:
         reversed = inputLine[::-1] #string slicing to reverse
-        close = reversed.find(')')
-        inputLine = inputLine[0, open] + calculate(inputLine[open + 1, input.len() - close - 1]) + inputLine(input.len() - close - 1, inputLine.len())
+        close = len(inputLine) - reversed.find(')')
+        inputLine = inputLine.replace(inputLine[open: close], calculate(inputLine[open + 1: close - 1]))
 
     done = False
     while done is False:
         done = True
         operatorIndex = inputLine.find('*' or '/')
-        if operatorIndex is not -1:
+        while operatorIndex is not -1:
+            operatorIndex = inputLine.find('*' or '/')
             operationSubstring = str(inputLine[operatorIndex - 1: operatorIndex + 2])
             done = False
             if inputLine[operatorIndex] is '*':
                 inputLine = inputLine.replace(operationSubstring, multiply(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
-                print(inputLine)
             elif inputLine[operatorIndex] is '/':
                 inputLine = inputLine.replace(operationSubstring, divide(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
-                print(inputLine)
-        else:
-            operatorIndex = inputLine.find('+' or '-')
-            if operatorIndex is not -1:
-                operationSubstring = str(inputLine[operatorIndex - 1: operatorIndex + 2])
-                done = False
-                if inputLine[operatorIndex] is '+':
-                    inputLine = inputLine.replace(operationSubstring, add(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
-                    print(inputLine)
-                elif inputLine[operatorIndex] is '-':
-                    inputLine = inputLine.replace(operationSubstring, subtract(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
-                    print(inputLine)
+            operatorIndex = inputLine.find('*' or '/')
+
+        operatorIndex = inputLine.find('+' or '-')
+        while operatorIndex is not -1:
+            operationSubstring = str(inputLine[operatorIndex - 1: operatorIndex + 2])
+            done = False
+            if inputLine[operatorIndex] is '+':
+                inputLine = inputLine.replace(operationSubstring, add(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
+            elif inputLine[operatorIndex] is '-':
+                inputLine = inputLine.replace(operationSubstring, subtract(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
+            operatorIndex = inputLine.find('*' or '/')
     return inputLine
-print(calculate(input))
+print(calculate(input()))
