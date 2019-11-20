fe4a14542e71c6a5b37bf20605ad91fab338cdc4
blazeris <blazeris@users.noreply.github.com>
2019-11-20 12:25:53 -0800

    worked on splitting string to terms

--- Calculator.py
+++ Calculator.py
@@ -12,40 +12,44 @@ def divide(firstTerm, secondTerm):
 
 def calculate(inputLine):
     inputLine = inputLine.replace(" ","")
-    operationOrder = {'*', '/', '+', '-'}
-    operations = {'*': multiply,
-                  '/': divide,
-                  '+': add,
-                  '-': subtract
-    }
+
+    #parentheses handling
     open = inputLine.find('(')
     if open is not -1:
-        reversed = inputLine[::-1] #string slicing to reverse
+        reversed = inputLine[::-1]
         close = len(inputLine) - reversed.find(')')
         inputLine = inputLine.replace(inputLine[open: close], calculate(inputLine[open + 1: close - 1]))
+
+    #line to individual terms
+    inputTerms = inputLine.split(" ")
 
     done = False
     while done is False:
         done = True
-        operatorIndex = inputLine.find('*' or '/')
-        while operatorIndex is not -1:
-            operatorIndex = inputLine.find('*' or '/')
-            operationSubstring = str(inputLine[operatorIndex - 1: operatorIndex + 2])
-            done = False
-            if inputLine[operatorIndex] is '*':
-                inputLine = inputLine.replace(operationSubstring, multiply(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
-            elif inputLine[operatorIndex] is '/':
-                inputLine = inputLine.replace(operationSubstring, divide(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
-            operatorIndex = inputLine.find('*' or '/')
 
-        operatorIndex = inputLine.find('+' or '-')
-        while operatorIndex is not -1:
-            operationSubstring = str(inputLine[operatorIndex - 1: operatorIndex + 2])
+        while ("*" or "/") in inputTerms:
+            operator_index = inputTerms.index("*" or "/")
             done = False
-            if inputLine[operatorIndex] is '+':
-                inputLine = inputLine.replace(operationSubstring, add(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
-            elif inputLine[operatorIndex] is '-':
-                inputLine = inputLine.replace(operationSubstring, subtract(inputLine[operatorIndex - 1], inputLine[operatorIndex + 1]))
-            operatorIndex = inputLine.find('*' or '/')
-    return inputLine
+            if inputLine[operator_index] is '*':
+                inputTerms[operator_index] = multiply(inputTerms[operator_index - 1], inputTerms[operator_index + 1])
+                inputTerms.pop(operator_index + 1)
+                inputTerms.pop(operator_index - 1)
+            elif inputLine[operator_index] is '/':
+                inputTerms[operator_index] = divide(inputLine[operator_index - 1], inputLine[operator_index + 1])
+                inputTerms.pop(operator_index + 1)
+                inputTerms.pop(operator_index - 1)
+
+        while ('+' or '-') in inputTerms:
+            operator_index = inputTerms.index('+' or '-')
+            done = False
+            if inputTerms[operator_index] is '+':
+                inputTerms[operator_index] = add(inputTerms[operator_index - 1], inputTerms[operator_index + 1])
+                inputTerms.pop(operator_index + 1)
+                inputTerms.pop(operator_index - 1)
+            elif inputLine[operator_index] is '-':
+                inputTerms[operator_index] = subtract(inputLine[operator_index - 1], inputLine[operator_index + 1])
+                inputTerms.pop(operator_index + 1)
+                inputTerms.pop(operator_index - 1)
+
+    return inputTerms[0]
 print(calculate(input()))
